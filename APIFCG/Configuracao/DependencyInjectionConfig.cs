108102b001d80e471f589797a4f63d900f508a1d
Gustavo00-dev <gustavorodrigues955@gmail.com>
2025-05-27 21:02:03 -0300

    init: Estrutura do projeto

---
+++ APIFCG/Configuracao/DependencyInjectionConfig.cs
@@ -0,0 +1,28 @@
+﻿using APIFCG.Infra.LogAPI;
+using APIFCG.Service;
+
+namespace APIFCG.Configuracao
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+   
+            services.AddHttpContextAccessor();
+            services.AddCorrelationIdGenerator();
+            services.AddTransient(typeof(BaseLogger<>));
+
+
+            #region Services/Repository   
+            services.AddScoped<IUsuarioService, UsuarioService>();
+            #endregion
+
+            #region Buider
+            //services.AddScoped<IDadosClienteBuilder, DadosClienteBuilder>();
+            #endregion
+
+
+            return services;
+        }
+    }
+}
